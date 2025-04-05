538ab8d978e82b51c3941d4aeab3fe97c7d16385
Binaryenigmaa <rc528611@gmail.com>
2025-04-05 18:02:51 +0530

    feat: Added All the message routes

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { isUserLoggedIn } from "../middleware/authMiddleware";
+import {
+  fetchMessages,
+  getSidebarUsers,
+  sendMessage,
+} from "../controllers/messageController";
 
 const messageRouter = express.Router();
+
+messageRouter.get("/users", isUserLoggedIn, getSidebarUsers);
+messageRouter.get("/:id", isUserLoggedIn, fetchMessages);
+messageRouter.post("/send/:id", isUserLoggedIn, sendMessage);
 
 export default messageRouter;
